0ef59e6e81f9651887d8afa01c6520d292867fa1
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-01-07 19:28:29 -0500

    #13 100% coverage

--- bets/tests.py
+++ bets/tests.py
@@ -27,7 +27,7 @@ class BetModelsTest(TestCase):
         self.assertEqual(self.event.__str__(), self.event.name)
         self.assertEqual(self.first_quota.__str__(), "{event} - {prob}".format(
             event=self.first_quota.event, prob=self.first_quota.probability))
-
+        
     def test_02_multiple_quotas(self):
         """
         This test evaluates creating Quotas from (in)active Quotas
@@ -35,11 +35,14 @@ class BetModelsTest(TestCase):
         
         # First quota should be inactive
         self.assertFalse(self.first_quota.active)
+
         # Second quota should be active
         self.assertTrue(self.second_quota.active)
+
         with self.assertRaises(ValidationError):
             # Should fail since first_quota is supposed to be inactive
             BetFactory(quota=self.first_quota)
+
         bet = BetFactory(quota=self.second_quota)
         # Bet should be created, and must have 'won=None' and 'active=True'
         self.assertIsInstance(bet, Bet)
@@ -50,7 +53,25 @@ class BetModelsTest(TestCase):
         self.assertIsNone(bet.won)
         self.assertTrue(bet.active)
 
-    def test_03_event_saving_completed(self):
+    def test_03_event_saving_incomplete(self):
+        """
+        This test evaluates the whole Event saving process
+        when the Event is still incomplete.
+        """
+
+        event = EventFactory(active=True)
+        event.save()
+        # Event must be incomplete
+        self.assertIsNone(event.completed)
+
+        quota = QuotaFactory(event=event, active=True)
+        event.active = False
+        event.save()
+        quota.refresh_from_db()
+        # Quota must be inactive
+        self.assertFalse(quota.active)
+
+    def test_04_event_saving_completed(self):
         """
         This test evaluates the whole Event saving process
         when the Event is completed.
@@ -69,14 +90,18 @@ class BetModelsTest(TestCase):
         # Both quotas should be inactive
         self.assertFalse(first_quota.active)
         self.assertFalse(second_quota.active)
+
         # Both bets should be won
         self.assertTrue(first_bet.won)
         self.assertTrue(second_bet.won)
+
         # Both bets should be inactive
         self.assertFalse(first_bet.active)
         self.assertFalse(second_bet.active)
+
         # Two prizes should be created
         self.assertEqual(Prize.objects.count(), 2)
+
         # Evaluate Prize __str__ representation
         prize = Prize.objects.first()
         self.assertEqual(prize.__str__(), "{event} - {user} - {amount}".format(
@@ -85,7 +110,7 @@ class BetModelsTest(TestCase):
             amount=prize.reward
         ))
 
-    def test_04_event_saving_not_completed(self):
+    def test_05_event_saving_not_completed(self):
         """
         This test evaluates the whole Event saving process
         when the Event is not completed.
@@ -104,11 +129,14 @@ class BetModelsTest(TestCase):
         # Both quotas should be inactive
         self.assertFalse(first_quota.active)
         self.assertFalse(second_quota.active)
+
         # Both bets should be lost 
         self.assertFalse(first_bet.won)
         self.assertFalse(second_bet.won)
+
         # Both bets should be inactive
         self.assertFalse(first_bet.active)
         self.assertFalse(second_bet.active)
+
         # No prizes should be created
         self.assertEqual(Prize.objects.count(), 0)

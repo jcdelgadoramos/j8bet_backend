4b5b1b34b22300907318ee3ddf381ff525513751
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-01-12 16:54:47 -0500

    #15 restoring settings

--- j8bet_backend/test_settings.py
+++ j8bet_backend/test_settings.py
@@ -1,6 +1,5 @@
 import environ
 import os
-from j8bet_backend.settings import *
 from pathlib import Path
 
 
@@ -14,6 +13,57 @@ ENV = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, "../.env"))
 
 # Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+LOCAL_APPS = [
+    "bets",
+]
+
+THIRD_PARTY_APPS = [
+    "graphene_django",
+]
+
+INSTALLED_APPS = INSTALLED_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+GRAPHENE = {
+    "SCHEMA": "j8bet_backend.graphql.api.schema",
+}
+
+ROOT_URLCONF = "j8bet_backend.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 CACHES = {
     'default': {

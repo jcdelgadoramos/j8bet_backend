517e200c1866bb210dcd296f2885721ae191ed9b
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-01-12 13:52:39 -0500

    #15 db settings action

--- j8bet_backend/test_settings.py
+++ j8bet_backend/test_settings.py
@@ -85,9 +85,9 @@ if SYSTEM_ENV == "GITHUB_WORKFLOW":
         "default": {
            "ENGINE": "django.db.backends.postgresql",
            "NAME": "test_db",
-           "USER": "test_user",
-           "PASSWORD": "test_pwd",
-           "HOST": "localhost",
+           "USER": "postgres",
+           "PASSWORD": "postgres",
+           "HOST": "127.0.0.1",
            "PORT": "5432",
         }
     }

e14dcc8534fa66f8884063cc69b65cead813226d
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-01-28 23:19:51 -0500

    #9 test fixes but one

--- users/graphql/queries.py
+++ users/graphql/queries.py
@@ -8,7 +8,7 @@ class UserQuery(ObjectType):
     user_by_id = Field(UserType, id=ID())
     user_by_username = Field(UserType, username=String())
 
-    def resolve_all_events(parent, info):
+    def resolve_all_users(parent, info):
         return get_user_model().objects.all()
 
     def resolve_user_by_id(parent, info, id):
--- users/tests.py
+++ users/tests.py
@@ -27,7 +27,7 @@ class QueryTest(TestCase):
         result = self.schema.execute(query)
         self.assertEqual(len(result.data["users"]), 1)
         self.assertEqual(
-            self.basic_user.username, result.data["users"][0]["name"]
+            self.basic_user.username, result.data["users"][0]["username"]
         )
 
     def test_02_get_single_user(self):
@@ -36,36 +36,36 @@ class QueryTest(TestCase):
         """
 
         query_by_id = """
-            query getUserById {
-                user: userById(id: {id}) {
+            query getUserById {{
+                user: userById(id: {id}) {{
                     id,
                     username,
                     email
-                }
-            }
+                }}
+            }}
         """.format(id=self.basic_user.id)
         result = self.schema.execute(query_by_id)
         self.assertEqual(
-            self.basic_user.id, result.data["user"]["id"]
+            self.basic_user.id, int(result.data["user"]["id"])
         )
         self.assertEqual(
-            self.basic_user.username, result.data["user"]["name"]
+            self.basic_user.username, result.data["user"]["username"]
         )
         query_by_username = """
-            query getUserByUsername {
-                user: userByUsername(username: {username}) {
+            query getUserByUsername {{
+                user: userByUsername(username: \"{username}\") {{
                     id,
                     username,
                     email
-                }
-            }
-        """.format(id=self.basic_user.username)
-        result = self.schema.execute(query_by_id)
+                }}
+            }}
+        """.format(username=self.basic_user.username)
+        result = self.schema.execute(query_by_username)
         self.assertEqual(
-            self.basic_user.id, result.data["user"]["id"]
+            self.basic_user.id, int(result.data["user"]["id"])
         )
         self.assertEqual(
-            self.basic_user.username, result.data["user"]["name"]
+            self.basic_user.username, result.data["user"]["username"]
         )
 
 
@@ -111,47 +111,46 @@ class CreateAuthenticateUserMutationTest(TestCase):
         self.assertEqual(username, executed["data"]["createUser"]["user"]["username"])
         self.assertEqual(email, executed["data"]["createUser"]["user"]["email"])
 
-#    def test_02_login_user(self):
-#        """
-#        This test reproduces the whole login process for a user.
-#        """
-#
-#        create_mutation = """
-#            mutation creatingANewUser(
-#                $username: String!,
-#                $email: String!,
-#                $password: String!
-#            ) {
-#                createUser (
-#                    username: $username,
-#                    email: $email,
-#                    password: $password
-#                ) {
-#                    user{
-#                        id,
-#                        username,
-#                        email,
-#                        password
-#                    }
-#                }
-#            }"""
-#        username = "username"
-#        password = "password"
-#        email = "user@j8.com"
-#        self.client.execute(
-#            create_mutation,
-#            variables = dict(username: username, email=email, password=password), 
-#        )
-#        get_token_mutation = """
-#            mutation getTokenAuth($username: String!, $password: String!) {
-#                tokenAuth(username: $username, password: $password) {
-#                    token
-#                }
-#            }
-#        """
-#        token_response = self.client.execute(
-#            get_token_mutation,
-#            variables = dict(username=username, password=password),
-#        )
-#        self.assertNotNull(token_response["data"]["tokenAuth"]["token"])
-#
+    def test_02_login_user(self):
+        """
+        This test reproduces the whole login process for a user.
+        """
+
+        create_mutation = """
+            mutation creatingANewUser(
+                $username: String!,
+                $email: String!,
+                $password: String!
+            ) {
+                createUser (
+                    username: $username,
+                    email: $email,
+                    password: $password
+                ) {
+                    user{
+                        id,
+                        username,
+                        email,
+                        password
+                    }
+                }
+            }"""
+        username = "username"
+        password = "password"
+        email = "user@j8.com"
+        creation_response = self.client.execute(
+            create_mutation,
+            variables = dict(username=username, email=email, password=password), 
+        )
+        get_token_mutation = """
+            mutation getTokenAuth($username: String!, $password: String!) {
+                tokenAuth(username: $username, password: $password) {
+                    token
+                }
+            }
+        """
+        token_response = self.client.execute(
+            get_token_mutation,
+            variables = dict(username=username, password=password),
+        )
+        self.assertIsNotNone(token_response["data"]["tokenAuth"]["token"])

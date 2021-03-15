5abb835e6e57c4f3c844ede510c64331d9782f48
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-03-15 16:34:41 -0500

    #28 improved me endpoint

--- users/graphql/queries.py
+++ users/graphql/queries.py
@@ -5,11 +5,18 @@ from users.graphql.types import UserType
 
 class UserObjectQuery(ObjectType):
     all_users = List(UserType)
+    me = Field(UserType)
     user_by_id = Field(UserType, id=ID())
     user_by_username = Field(UserType, username=String())
 
     def resolve_all_users(parent, info):
         return get_user_model().objects.all()
+
+    def resolve_me(parent, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return user
+        return None
 
     def resolve_user_by_id(parent, info, id):
         return get_user_model().objects.get(id=id)
--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import Group
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from graphene import Schema
 from graphene.test import Client
 from graphql_jwt.testcases import JSONWebTokenTestCase
+from j8bet_backend.constants import BET_CONSUMER
 from users.factories import UserFactory
 from users.graphql.schema import Mutation as UserMutation
 from users.graphql.schema import Query as UserQuery
@@ -11,6 +13,8 @@ from users.graphql.schema import Query as UserQuery
 class QueryTest(JSONWebTokenTestCase):
     def setUp(self):
         self.user = UserFactory()
+        self.bet_consumer_group = Group.objects.get(name=BET_CONSUMER)
+        self.user = UserFactory.create(groups=(self.bet_consumer_group,))
         self.client.authenticate(self.user)
         super().setUp()
 
@@ -27,9 +31,9 @@ class QueryTest(JSONWebTokenTestCase):
             }
         """
         result = self.client.execute(query)
-        self.assertEqual(len(result.data["users"]), 1)
-        self.assertEqual(
-            self.user.username, result.data["users"][0]["username"]
+        self.assertEqual(len(result.data["users"]), 2)
+        self.assertIn(
+            self.user.username, [user["username"] for user in result.data["users"]]
         )
 
     def test_02_get_single_user(self):
@@ -65,6 +69,46 @@ class QueryTest(JSONWebTokenTestCase):
         result = self.client.execute(query_by_username)
         self.assertEqual(self.user.id, int(result.data["user"]["id"]))
         self.assertEqual(self.user.username, result.data["user"]["username"])
+
+    def test_03_me(self):
+        """
+        This test evaluates the 'me' query.
+        """
+
+        query = """
+            query{
+                me{
+                    username,
+                    archived,
+                    verified,
+                    secondaryEmail,
+                    groups{
+                        name,
+                    }
+                }
+            }
+        """
+        result = self.client.execute(query)
+        self.assertEqual(
+            self.user.username, result.data["me"]["username"]
+        )
+        self.assertEqual(
+            self.user.status.archived, result.data["me"]["archived"]
+        )
+        self.assertEqual(
+            self.user.status.verified, result.data["me"]["verified"]
+        )
+        self.assertEqual(
+            self.user.status.secondary_email,
+            result.data["me"]["secondaryEmail"],
+        )
+        self.assertEqual(
+            self.user.groups.first().name,
+            result.data["me"]["groups"][0]["name"],
+        )
+        self.client.logout()
+        result = self.client.execute(query)
+        self.assertIsNone(result.data["me"])
 
 
 class CreateUserMutationTest(TestCase):
